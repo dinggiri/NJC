7bfd8467316368849c16e19d17745a5000d56105
yoonseonchoi <dbstjs1008@gmail.com>
2023-01-09 19:37:56 +0900

    토글 완성

--- Website/js/실전사례 검색.js
+++ Website/js/실전사례 검색.js
@@ -2,10 +2,31 @@ $(function (){
     $('input[type="radio"][name="mix"]').on('click', function(){
         var chkValue = $('input[type=radio][name="mix"]:checked').val();
         if(chkValue == 'yes'){
-            $('#details_item').css('display','block');
-        }else if(chkValue == 'no'){
-            $('#details_item').css('display','none');
+            $('#white_item').css('display','block');
+        }else{
+            $('#white_item').css('display','none');
         }
     });
+    $('input[type="checkbox"][name="reason"][value="유색오염"]').on('click', function(){
+        var chkValue = $('input[type=checkbox][name="reason"][value="유색오염"]:checked').val();
+        if(chkValue == '유색오염'){
+            $('#col_reason_item').css('display','block');
+        }else{$('#col_reason_item').css('display','none');
+        }
     });
+    $('input[type="checkbox"][name="reason"][value="생활얼룩"]').on('click', function(){
+        var chkValue = $('input[type=checkbox][name="reason"][value="생활얼룩"]:checked').val();
+        if(chkValue == '생활얼룩'){
+            $('#blemish_item').css('display','block');
+        }else{$('#blemish_item').css('display','none');
+        }
+    });
+    $('input[type="checkbox"][name="reason"][value="기타"]').on('click', function(){
+        var chkValue = $('input[type=checkbox][name="reason"][value="기타"]:checked').val();
+        if(chkValue == '기타'){
+            $('#etc_item').css('display','block');
+        }else{$('#etc_item').css('display','none');
+        }
+    });
+});
      
