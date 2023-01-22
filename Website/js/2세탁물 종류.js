94fb2ca1553b2df2dd08326a414c42692e061bbb
yoonseonchoi <dbstjs1008@gmail.com>
2023-01-22 20:18:20 +0900

    스파게티 수정

--- Website/js/2세탁물 종류.js
+++ Website/js/2세탁물 종류.js
@@ -2,12 +2,10 @@ $(function (){
     $('input[type="checkbox"][name="type"][value="가방"]').on('click', function(){
         var chkValue = $('input[type=checkbox][name="type"]:checked').val();
         if(chkValue == '가방'){
-            $('.next').attr('onclick',"location.href='4색상.html'");
-            $('.prev1').attr('onclick',"location.href='2세탁물 종류.html'");
+            $('.next').attr('onclick',"location.href='4.1색상.html'");
         }
         else{
             $('.next').attr('onclick',"location.href='3소재.html'");
-            
         }
     });
     $('input[type="checkbox"][name="type"][value="신발"]').on('click', function(){
--- Website/js/4색상.js
+++
@@ -1,5 +0,0 @@
-$(function (){
-    $('.prev1').on('click', function(){   
-        this.attr('onclick',"location.href='3소재.html'");
-    });
-});
