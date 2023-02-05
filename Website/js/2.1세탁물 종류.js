33d0f8a9a5b4aed8de30a14c95bd01dfabd5e30c
m00nsu <102724311+m00nsu@users.noreply.github.com>
2023-02-05 18:41:10 +0900

    2/05 zero-checked alert edit
    return false;
    중복 체크 X 버전

---
+++ Website/js/2.1세탁물 종류.js
@@ -0,0 +1,9 @@
+$(function (){
+    $('.next').on('click', function(){
+        var chkLength = $('input[type=radio][name="type"]:checked').length;
+        if(chkLength < 1){
+            alert("하나 이상 체크해주세요");
+            return false;
+        }
+    });
+});
