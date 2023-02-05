2f66be3cd798e6542eb71bcf8871fa03ed8eb434
m00nsu <102724311+m00nsu@users.noreply.github.com>
2023-02-05 18:44:30 +0900

    2/05 zero-checked alert edit
    return false;

--- Website/js/5세탁 사유.js
+++ Website/js/5세탁 사유.js
@@ -1,4 +1,11 @@
 $(function (){
+    $('.next').on('click', function(){
+        var chkLength = $('input[type=checkbox][name="reason"]:checked').length;
+        if(chkLength < 1){
+            alert("하나 이상 체크해주세요");
+            return false;
+        }
+    });
     $('input[type="checkbox"][name="reason"][value="유색오염"]').on('click', function(){
         var chkValue = $('input[type=checkbox][name="reason"][value="유색오염"]:checked').val();
         if(chkValue == '유색오염'){
@@ -18,12 +25,6 @@ $(function (){
         if(chkValue == '기타'){
             $('#etc_item').css('display','block');
         }else{$('#etc_item').css('display','none');
-        }
-    });
-    $('.next').on('click', function(){
-        var chkLength = $('input[type=checkbox][name="reason"]:checked').length;
-        if(chkLength < 1){
-            alert("하나 이상 체크해주세요");
         }
     });
 });
