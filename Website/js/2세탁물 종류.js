221e6b440bec1a0d929a0dd1f107738c0c867e48
m00nsu <102724311+m00nsu@users.noreply.github.com>
2023-02-05 18:41:51 +0900

    2/05 zero-checked alert edit

--- Website/js/2세탁물 종류.js
+++ Website/js/2세탁물 종류.js
@@ -1,32 +1,9 @@
 $(function (){
-    $('input[type="checkbox"][name="type"][value="가방"]').on('click', function(){
-        var chkValue = $('input[type=checkbox][name="type"]:checked').val();
-        if(chkValue == '가방'){
-            $('.next').attr('onclick',"location.href='4.1색상.html'");
-        }
-        else{
-            $('.next').attr('onclick',"location.href='3소재.html'");
-        }
-    });
-    $('input[type="checkbox"][name="type"][value="신발"]').on('click', function(){
-        var chkValue = $('input[type=checkbox][name="type"]:checked').val();
-        if(chkValue == '신발'){
-            $('.next').attr('onclick',"location.href='3.1신발.html'");
-        }
-        else {
-            $('.next').attr('onclick',"location.href='3소재.html'");
-        }
-    });
-    $('input[type="checkbox"][name="type"]').on('click', function(){
-        var chkLength = $('input[type=checkbox][name="type"]:checked').length;
-        if(chkLength > 1){
-            $('.next').attr('onclick',"location.href='3소재.html'");
-        }
-    });
     $('.next').on('click', function(){
         var chkLength = $('input[type=checkbox][name="type"]:checked').length;
         if(chkLength < 1){
             alert("하나 이상 체크해주세요");
+            return false;
         }
     });
 });
