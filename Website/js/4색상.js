0bd7a823a2f1862f8cea3ba109f08f7801d86ed8
m00nsu <102724311+m00nsu@users.noreply.github.com>
2023-02-05 18:44:04 +0900

    2/05 zero-checked alert edit +@
    return false;
    Deleted checkbox part

--- Website/js/4색상.js
+++ Website/js/4색상.js
@@ -3,12 +3,7 @@ $(function (){
         var chkLength = $('input[type=radio][name="color"]:checked').length;
         if(chkLength < 1){
             alert("하나 이상 체크해주세요");
-        }
-    });
-    $('.next').on('click', function(){
-        var chkLength2 = $('input[type=checkbox][name="color"]:checked').length;
-        if(chkLength2 < 1){
-            alert("하나 이상 체크해주세요");
+            return false;
         }
     });
 });
