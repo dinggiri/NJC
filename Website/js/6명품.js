47090d15b6f03c5b8e5b92bcf7752aa16b1b79bb
m00nsu <102724311+m00nsu@users.noreply.github.com>
2023-02-05 18:44:55 +0900

    2/05 zero-checked alert edit
    return false;

--- Website/js/6명품.js
+++ Website/js/6명품.js
@@ -3,6 +3,7 @@ $(function (){
         var chkLength = $('input[type=radio][name="named"]:checked').length;
         if(chkLength < 1){
             alert("하나 이상 체크해주세요");
+            return false;
         }
     });
 });
