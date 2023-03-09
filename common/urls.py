67ae851cd717583d37a5b62a8fe243e00d93e779
d0al <moons@hanyang.ac.kr>
2023-03-09 18:02:26 +0900

    로그인 횟수

--- common/urls.py
+++ common/urls.py
@@ -6,6 +6,6 @@ app_name = 'common'
 
 urlpatterns = [
     path('', views.title, name='title'),
-    path('login/', auth_views.LoginView.as_view(template_name='common/Login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(form_class=CustomAuthenticationForm, template_name='common/Login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
